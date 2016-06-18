d820045cc0e3201bb2693ed7c157620ce76942d2
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-06-18 10:25:31 +0700

    Bổ sung ảnh ruby, label. Thực hiện animation

--- iAmRich/ViewController.m
+++ iAmRich/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imgRuby;
+@property (weak, nonatomic) IBOutlet UILabel *lblQuote;
 
 @end
 
@@ -17,11 +19,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _imgRuby.alpha = 0;
+    _lblQuote.alpha = 0;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [UIView animateWithDuration:4 animations:^{
+        self.imgRuby.alpha = 1;
+        
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:3 animations:^{
+            
+            self.lblQuote.center = CGPointMake(self.lblQuote.center.x, 360);
+            self.lblQuote.alpha = 1;
+            
+        } completion:nil];
+    }];
 }
 
 @end
